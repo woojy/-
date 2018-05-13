158c50cbc7e24c4e173f804b98b754f0fb75a460
Jaeyun Woo <woojy3770@e-mirim.hs.kr>
2018-05-14 07:24:23 +0900

    #1

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace _3111_우재윤
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(61, 231);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(254, 29);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightPink;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(138, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "암호키 입력";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightPink;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(147, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "평문 입력";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightPink;
+            this.label3.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(119, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "단일치환 결과";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("굴림", 10F);
+            this.button1.ForeColor = System.Drawing.Color.DeepPink;
+            this.button1.Location = new System.Drawing.Point(70, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "암호화 하기";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("굴림", 10F);
+            this.button2.ForeColor = System.Drawing.Color.DeepPink;
+            this.button2.Location = new System.Drawing.Point(190, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "초기화";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightPink;
+            this.label4.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(127, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "복호화 결과";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox4.Location = new System.Drawing.Point(61, 305);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(254, 29);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(374, 348);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _3111_우재윤
+{
+    public partial class Form1 : Form
+    {
+        List<char> alpha = new List<char>(); //알파벳 a~z 까지 저장될 변수
+        List<char> amhokey = new List<char>(); //암호키가 저장될 변수
+        List<char> result = new List<char>(); //중복제거된 암호키가 저장될 리스트
+        List<char> ch = new List<char>();//평문이 하나 하나 저장될 char 리스트
+        List<char> sub = new List<char>(); //치환된 암호문이 저장될 리스트
+        List<char> bok = new List<char>(); //복호화된 암호문이 저장될 리스트 
+        List<char> plain; //복호화된 암호문이 저장될 리스트  s
+
+        string amho="";
+        string plainText = ""; //평문
+        string cryptogram = ""; //암호문이 찍힐 textbox
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            amho = textBox1.Text;
+            plainText = textBox2.Text; //평문
+            cryptogram = textBox3.Text; //암호문이 찍힐 textbox
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                //알파벳을 순서대로 List에 넣어줌
+                alpha.Add(i);
+            }
+            amho = amho.ToLower(); //암호키 모두 소문자
+            plainText = plainText.ToLower(); //얘도
+            plainText = plainText.Trim();//얘는 평문 공백지우기
+
+            for (int i = 0; i < amho.Length; i++)
+            {
+                //암호키를 result 리스트에 넣어주기
+                result.Add(amho[i]);
+                result = result.Distinct().ToList();//암호키 중복제거
+            }
+            for (char c = result.LastOrDefault(); c <= 'z'; c++) //마지막 알파벳부터 'z'까지 넣기
+            {
+                result.Add(c);
+                result = result.Distinct().ToList();//암호문 중복제거
+            }
+
+            if (result.Count < 26) //만약 암호문이 꽉차지 않았으면
+            {
+                for (char c = 'a'; c <= 'z'; c++) //다시 a부터 넣어라
+                {
+                    result = result.Distinct().ToList();//암호문 중복제거
+                    result.Add(c); //나머지 뒤에 쫘르륵 넣기
+                }
+            }
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                //평문의 알파벳 하나 하나 list에 저장
+                ch.Add(plainText[i]);
+            }
+
+            for (int i = 0; i < ch.Count; i++) //평문에 맞는 index가져오기
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (ch[i] == alpha[j]) //평문 [i] 랑 alpha[i] 비교해서 찾으면
+                    {
+                        sub.Add(result[j]); //sub 배열에 암호판 [i]넣기
+                    }
+                }
+            }
+            foreach (char i in sub) //암호문 텍스트박스에 암호화된 문자찍기
+            {
+                textBox3.Text += i;
+            }
+
+            for (int i = 0; i < sub.Count; i++) //암호화문에 맞는 alpha가져오기
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (sub[i] == result[j])
+                    {
+                        bok.Add(alpha[j]);
+                    }
+                }
+            }
+            foreach (char i in bok) //암호문 텍스트박스에 복호화된 문자찍기
+            {
+                textBox4.Text += i;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            alpha.Clear();
+            amhokey.Clear(); //암호키가 저장될 변수
+            result.Clear(); //중복제거된 암호키가 저장될 리스트
+            ch.Clear();//평문이 하나 하나 저장될 char 리스트
+            sub.Clear(); //치환된 암호문이 저장될 리스트
+        }
+    }
+}
